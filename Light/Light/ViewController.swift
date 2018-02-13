698df99b230768fb5d2f7c09bf038b581912eb3f
Joe Sturzenegger <joesturz84@gmail.com>
2018-02-12 22:04:29 -0700

    add code to turn the light on and off

--- Light/Light/ViewController.swift
+++ Light/Light/ViewController.swift
@@ -12,14 +12,35 @@ class ViewController: UIViewController {
 
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
+    updateUI()
   }
-
+  
   override func didReceiveMemoryWarning() {
     super.didReceiveMemoryWarning()
     // Dispose of any resources that can be recreated.
   }
-
-
+  
+  @IBOutlet weak var lightButton: UIButton!
+  
+  var lightOn = true
+  
+  @IBAction func buttonPressed(_ sender: Any) {
+    lightOn = !lightOn
+    updateUI()
+  }
+  
+  func updateUI()
+  {
+    if lightOn
+    {
+      view.backgroundColor = .white
+      lightButton.setTitle("Off", for: .normal)
+    }
+    else
+    {
+      view.backgroundColor = .black
+      lightButton.setTitle("On", for: .normal)
+    }
+  }
 }
 
