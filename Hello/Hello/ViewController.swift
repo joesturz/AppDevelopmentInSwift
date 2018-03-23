9e55cda3c9220f6a24b28ac96dbc48585b4b6641
Joe Sturzenegger <joesturz84@gmail.com>
2018-03-22 20:16:12 -0600

    created the Hello project completed part 1

---
+++ Hello/Hello/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Hello
+//
+//  Created by Joe Sturzenegger on 3/22/18.
+//  Copyright © 2018 Joe Sturzenegger. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
