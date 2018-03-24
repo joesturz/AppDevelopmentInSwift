b300d5415e146785071a7d05e8ef58e1a3c744e8
Joe Sturzenegger <joesturz84@gmail.com>
2018-03-24 11:39:59 -0600

    Finished Hobby Tutorial

---
+++ Hobby Tutorial/Hobby Tutorial/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Hobby Tutorial
+//
+//  Created by Joe Sturzenegger on 3/24/18.
+//  Copyright © 2018 Joe Sturzenegger. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
